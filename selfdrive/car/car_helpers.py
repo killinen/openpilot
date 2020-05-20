4239fd64cdc7eb267b1d2d5871efdb025bcc5b86
SelfdriveVanagon <41362285+Lukilink@users.noreply.github.com>
2020-05-20 12:31:45 +0200

    Update car_helpers.py

--- selfdrive/car/car_helpers.py
+++ selfdrive/car/car_helpers.py
@@ -119,7 +119,7 @@ def get_car(logcan, sendcan, has_relay=False):
 
   if candidate is None:
     cloudlog.warning("car doesn't match any fingerprints: %r", fingerprints)
-    candidate = "mock"
+    candidate = "CAR.OLD_CAR"
 
   CarInterface, CarController = interfaces[candidate]
   car_params = CarInterface.get_params(candidate, fingerprints, has_relay, car_fw)
