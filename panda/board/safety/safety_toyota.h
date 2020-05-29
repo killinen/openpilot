6e379dcd225b89c3b0eede344f1fd0bcd25ff1c3
killinen <37126045+killinen@users.noreply.github.com>
2020-05-29 12:54:44 +0300

    Update safety_toyota.h
    Added >BMW  messages to TOYOTA_TX_MSGS[]

--- panda/board/safety/safety_toyota.h
+++ panda/board/safety/safety_toyota.h
@@ -32,12 +32,12 @@ const int TOYOTA_GAS_INTERCEPTOR_THRSLD = 845;
 const AddrBus TOYOTA_TX_MSGS[] = {{0x283, 0}, {0x2E6, 0}, {0x2E7, 0}, {0x33E, 0}, {0x344, 0}, {0x365, 0}, {0x366, 0}, {0x4CB, 0},  // DSU bus 0
                                   {0x128, 1}, {0x141, 1}, {0x160, 1}, {0x161, 1}, {0x470, 1},  // DSU bus 1
                                   {0x2E4, 0}, {0x411, 0}, {0x412, 0}, {0x343, 0}, {0x1D2, 0},  // LKAS + ACC
-                                  {0x200, 0}, {0x750, 0}};  // interceptor + Blindspot monitor
+                                  {0x200, 0}, {0x750, 0}, {0x1F0, 0}, {0x153, 0}, {0x43F, 0}, {0x329, 0}, {0x615, 0}};  // interceptor + Blindspot monitor
 
 AddrCheckStruct toyota_rx_checks[] = {
   {.addr = {0x1F0}, .bus = 0, .check_checksum = false, .expected_timestep = 12000U},
   {.addr = {0x153}, .bus = 0, .check_checksum = false, .expected_timestep = 20000U},
-  {.addr = {0x329}, .bus = 0, .check_checksum = false, .expected_timestep = 30000U},
+  {.addr = {0x43F}, .bus = 0, .check_checksum = false, .expected_timestep = 30000U},
   {.addr = {0x224, 0x329}, .bus = 0, .check_checksum = false, .expected_timestep = 25000U},
 };
 const int TOYOTA_RX_CHECKS_LEN = sizeof(toyota_rx_checks) / sizeof(toyota_rx_checks[0]);
