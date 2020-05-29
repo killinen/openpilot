ee61b2c126ac6314cf2e5fa31f88c390e202423d
killinen <37126045+killinen@users.noreply.github.com>
2020-05-29 17:35:29 +0300

    Update safety_toyota.h
    Changed brake pressed decimal value to hex and test USB_DEBUG

--- panda/board/safety/safety_toyota.h
+++ panda/board/safety/safety_toyota.h
@@ -115,13 +115,18 @@ static int toyota_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
         speed += ((GET_BYTE(to_push, next_byte) & 0x0F) << 8) + (GET_BYTE(to_push, i)) - 0x2A;
       }
       vehicle_moving = ABS(speed / 4) > TOYOTA_STANDSTILL_THRSLD;
+      if (vehicle_moving == true) {
+        #ifdef DEBUG_USB
+          puts(" set address\n");
+        #endif
+      }
     }
 
     // exit controls on rising edge of brake pedal
     // most cars have brake_pressed on 0x329, corolla and rav4 on 0x224
     if ((addr == 0x224) || (addr == 0x329)) {
       int byte = (addr == 0x224) ? 0 : 6;
-      bool brake_pressed = ((GET_BYTE(to_push, byte) << 7) & 80) != 0;
+      bool brake_pressed = ((GET_BYTE(to_push, byte) << 7) & 0x80) != 0;
       if (brake_pressed && (!brake_pressed_prev || vehicle_moving)) {
         controls_allowed = 0;
       }
