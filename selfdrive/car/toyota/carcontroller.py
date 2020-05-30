a1bdef7b1981dbaa979cfd0e7a2de50dbebe08f4
killinen <37126045+killinen@users.noreply.github.com>
2020-05-30 16:27:37 +0300

    Update carcontroller.py
    Test to line 79 fingerprint and gave few options

--- selfdrive/car/toyota/carcontroller.py
+++ selfdrive/car/toyota/carcontroller.py
@@ -76,7 +76,9 @@ class CarController():
       self.last_fault_frame = frame
 
     # Cut steering for 2s after fault
-    if not enabled or self.CS.carFingerprint in NO_EPS_CAR or (frame - self.last_fault_frame < 200):
+    #if not enabled or self.CS.CP.carFingerprint in NO_EPS_CAR or (frame - self.last_fault_frame < 200):
+    if not enabled or CS.CP.carFingerprint in NO_EPS_CAR or (frame - self.last_fault_frame < 200):
+    #if not enabled or self.CS.carFingerprint in NO_EPS_CAR or (frame - self.last_fault_frame < 200):
     #if not enabled or (frame - self.last_fault_frame < 200):
       apply_steer = 0
       apply_steer_req = 0
