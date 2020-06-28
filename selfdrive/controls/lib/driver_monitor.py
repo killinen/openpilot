054087041b6b1782f97bdfedc46bf11e50b5f6e5
killinen <37126045+killinen@users.noreply.github.com>
2020-06-28 23:23:46 +0300

    Add files via upload
    Own version

--- selfdrive/controls/lib/driver_monitor.py
+++ selfdrive/controls/lib/driver_monitor.py
@@ -11,14 +11,14 @@ from common.stat_live import RunningStatFilter
 #  We recommend that you do not change these numbers from the defaults.
 # ******************************************************************************************
 
-_AWARENESS_TIME = 70.  # one minute limit without user touching steering wheels make the car enter a terminal status
+_AWARENESS_TIME = 350.  # 3 minute limit without user touching steering wheels make the car enter a terminal status
 _AWARENESS_PRE_TIME_TILL_TERMINAL = 15.  # a first alert is issued 25s before expiration
 _AWARENESS_PROMPT_TIME_TILL_TERMINAL = 6.  # a second alert is issued 15s before start decelerating the car
 _DISTRACTED_TIME = 11.
 _DISTRACTED_PRE_TIME_TILL_TERMINAL = 8.
 _DISTRACTED_PROMPT_TIME_TILL_TERMINAL = 6.
 
-_FACE_THRESHOLD = 0.4
+_FACE_THRESHOLD = 0.2
 _EYE_THRESHOLD = 0.6
 _BLINK_THRESHOLD = 0.5 # 0.225
 _BLINK_THRESHOLD_SLACK = 0.65
@@ -224,8 +224,8 @@ class DriverStatus():
     driver_attentive = self.driver_distraction_filter.x < 0.37
     awareness_prev = self.awareness
 
-    if self.face_detected and self.hi_stds * DT_DMON > _HI_STD_TIMEOUT:
-      events.append(create_event('driverMonitorLowAcc', [ET.WARNING]))
+#     if self.face_detected and self.hi_stds * DT_DMON > _HI_STD_TIMEOUT:
+#       events.append(create_event('driverMonitorLowAcc', [ET.WARNING]))
 
     if (driver_attentive and self.face_detected and self.pose.low_std and self.awareness > 0):
       # only restore awareness when paying attention and alert is not red
