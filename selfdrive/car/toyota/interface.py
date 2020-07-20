cb16d8f7d37702ecf3cd695f81ca91114016671a
killinen <37126045+killinen@users.noreply.github.com>
2020-07-20 22:55:13 +0300

    Update interface.py
    Maxed out gasMaxV because made rudimentary wheeltorque control in Bedalduino

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -283,7 +283,8 @@ class CarInterface(CarInterfaceBase):
 
     if ret.enableGasInterceptor:
       ret.gasMaxBP = [0., 15., 25]
-      ret.gasMaxV = [0.5, 0.5, 0.7]
+      # ret.gasMaxV = [0.5, 0.5, 0.7]      
+      ret.gasMaxV = [1.0, 1.0, 1.0]
       ret.longitudinalTuning.kpV = [1.0, 0.8, 0.6]
       ret.longitudinalTuning.kiV = [0.15, 0.1]
 #       ret.gasMaxBP = [0., 9., 35]   # Original values
