bcf455a988141c56a8ef0f1e8697d7b9ba00c7f9
killinen <37126045+killinen@users.noreply.github.com>
2020-10-23 20:46:35 +0300

    Change ret.gasMaxV = [.6, .8, 1.0]

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -287,7 +287,7 @@ class CarInterface(CarInterfaceBase):
     if ret.enableGasInterceptor:
       ret.gasMaxBP = [0., 15., 25]
       # ret.gasMaxV = [0.5, 0.5, 0.7]      
-      ret.gasMaxV = [.15, .4, .8]
+      ret.gasMaxV = [.6, .8, 1.0]
       ret.longitudinalTuning.kpV = [1.0, 0.8, 0.6]
       ret.longitudinalTuning.kiV = [0.15, 0.1]
 #       ret.gasMaxBP = [0., 9., 35]   # Original values
