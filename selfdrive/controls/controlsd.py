4185df1a883e93e0763cbed78825d9fdef669d60
killinen <37126045+killinen@users.noreply.github.com>
2021-07-21 19:47:12 +0300

    Revert WPanda support II

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -55,8 +55,7 @@ class Controls:
 
     self.sm = sm
     if self.sm is None:
-      ignore = ['ubloxRaw'] if NOSENSOR else None
-      ignore = ['ubloxRaw', 'driverCameraState', 'managerState'] if SIMULATION else ignore
+      ignore = ['ubloxRaw', 'driverCameraState', 'managerState'] if SIMULATION else None
 
       self.sm = messaging.SubMaster(['deviceState', 'pandaState', 'modelV2', 'liveCalibration', 'ubloxRaw',
                                      'driverMonitoringState', 'longitudinalPlan', 'lateralPlan', 'liveLocationKalman',
