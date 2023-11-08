ab49137a53a68f556519b70a288db9bdc2bd2f88
killinen <37126045+killinen@users.noreply.github.com>
2023-11-08 23:07:54 +0200

    Update registration.py
    Try to disable device registration on installation

--- selfdrive/athena/registration.py
+++ selfdrive/athena/registration.py
@@ -77,9 +77,29 @@ def register(show_spinner=False) -> str:
         cloudlog.exception("failed to authenticate")
         backoff = min(backoff + 1, 15)
         time.sleep(backoff)
-
-      if time.monotonic() - start_time > 60 and show_spinner:
-        spinner.update(f"registering device - serial: {serial}, IMEI: ({imei1}, {imei2})")
+    
+    
+#    while True:
+#      try:
+#        register_token = jwt.encode({'register': True, 'exp': datetime.utcnow() + timedelta(hours=1)}, private_key, algorithm='RS256')
+#        cloudlog.info("getting pilotauth")
+#        resp = api_get("v2/pilotauth/", method='POST', timeout=15,
+#                       imei=imei1, imei2=imei2, serial=serial, public_key=public_key, register_token=register_token)
+#
+#        if resp.status_code in (402, 403):
+#          cloudlog.info(f"Unable to register device, got {resp.status_code}")
+#          dongle_id = UNREGISTERED_DONGLE_ID
+#        else:
+#          dongleauth = json.loads(resp.text)
+#          dongle_id = dongleauth["dongle_id"]
+#        break
+#      except Exception:
+#        cloudlog.exception("failed to authenticate")
+#        backoff = min(backoff + 1, 15)
+#        time.sleep(backoff)
+#
+#      if time.monotonic() - start_time > 60 and show_spinner:
+#        spinner.update(f"registering device - serial: {serial}, IMEI: ({imei1}, {imei2})")
 
     if show_spinner:
       spinner.close()
