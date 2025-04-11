c24940ea3b8dc7cd251fcd13ebd9136e0c720e77
killinen <37126045+killinen@users.noreply.github.com>
2025-04-11 09:21:38 +0300

    Update registration.py
    Try to ship regitration for testing purposes

--- selfdrive/athena/registration.py
+++ selfdrive/athena/registration.py
@@ -61,6 +61,10 @@ def register(show_spinner=False) -> str:
     start_time = time.monotonic()
     while True:
       try:
+        if time.monotonic() - start_time > 60:
+          cloudlog.warning("Server registration timeout, using unregistered ID")
+          dongle_id = UNREGISTERED_DONGLE_ID
+          break
         register_token = jwt.encode({'register': True, 'exp': datetime.utcnow() + timedelta(hours=1)}, private_key, algorithm='RS256')
         cloudlog.info("getting pilotauth")
         resp = api_get("v2/pilotauth/", method='POST', timeout=15,
