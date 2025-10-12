11c4c74e30b525445ef54a28567cb5f45627f274
James <91348155+FrogAi@users.noreply.github.com>
2025-10-11 18:53:36 -0700

    FrogPilot branding

--- selfdrive/ui/soundd.py
+++ selfdrive/ui/soundd.py
@@ -42,6 +42,7 @@ sound_list: dict[int, tuple[str, int | None, float]] = {
   AudibleAlert.warningImmediate: ("warning_immediate.wav", None, MAX_VOLUME),
 
   # FrogPilot variables
+  FrogPilotAudibleAlert.startup: ("startup.wav", 1, MAX_VOLUME),
 }
 
 def check_selfdrive_timeout_alert(sm):
