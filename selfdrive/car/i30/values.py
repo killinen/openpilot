9f07980adc3295c074b2183d74fd4df451919c71
killinen <bhepatiitti@gmail.com>
2026-06-13 16:19:42 +0300

    Ramp up max steer torque to 800 -> 1000 cNm

--- selfdrive/car/i30/values.py
+++ selfdrive/car/i30/values.py
@@ -57,11 +57,11 @@ class SteerLimitParams:  # controls running @ 100hz
 class TrqiSteerLimitParams:  # controls running @ 100hz
   # 0x232 uses signed desired EPS output torque in Ncm. Keep the openpilot-side
   # range aligned with the existing ~4 Nm steering window.
-  MAX_STEERING_TQ = 800.0
+  MAX_STEERING_TQ = 1000.0
   STEER_DELTA_UP = 6.0
   STEER_DELTA_DOWN = 6.0
   STEER_STEP = 1
-  STEER_MAX = 800.0
+  STEER_MAX = 1000.0
 
   RELAY_ENABLED = 1
   RELAYE_ENABLED = 1
