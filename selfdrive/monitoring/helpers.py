2f0bd74a0e508f50fcc71961c11350b0dac4295c
killinen <bhepatiitti@gmail.com>
2026-08-14 20:13:22 +0300

    Increase DM time

--- selfdrive/monitoring/helpers.py
+++ selfdrive/monitoring/helpers.py
@@ -24,9 +24,9 @@ class DRIVER_MONITOR_SETTINGS:
     self._AWARENESS_TIME = 30. # passive wheeltouch total timeout
     self._AWARENESS_PRE_TIME_TILL_TERMINAL = 15.
     self._AWARENESS_PROMPT_TIME_TILL_TERMINAL = 6.
-    self._DISTRACTED_TIME = 11. # active monitoring total timeout
-    self._DISTRACTED_PRE_TIME_TILL_TERMINAL = 8.
-    self._DISTRACTED_PROMPT_TIME_TILL_TERMINAL = 6.
+    self._DISTRACTED_TIME = 22. # active monitoring total timeout
+    self._DISTRACTED_PRE_TIME_TILL_TERMINAL = 16.
+    self._DISTRACTED_PROMPT_TIME_TILL_TERMINAL = 12.
 
     self._FACE_THRESHOLD = 0.7
     self._EYE_THRESHOLD = 0.65
